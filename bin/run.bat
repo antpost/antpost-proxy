87880f9bc00835f1976e839289d9f5c982de3cca
Cuong Nguyen Manh <Cuong Nguyen Manh>
2017-05-28 13:07:32 +0700

    update

--- bin/run.bat
+++ bin/run.bat
@@ -2,7 +2,7 @@
 @setlocal enableextensions
 @cd /d "%~dp0"
 
-nssm64 install antpost_proxy %CD%\antpost-proxy.exe
+nssm64 install antpost_proxy antpost-proxy.exe
 nssm64 start antpost_proxy
 
 pause
